3db0dc22d21dac705a4d08351a5ca70f620e2541
David Nurkkala <dnurkkala@gmail.com>
2023-11-16 01:55:41 -0500

    number popup

---
+++ src/shared/React/NumberPopups/NumberPopup.lua
@@ -0,0 +1,62 @@
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+
+local Container = require(ReplicatedStorage.Shared.React.Common.Container)
+local Flipper = require(ReplicatedStorage.Packages.Flipper)
+local Image = require(ReplicatedStorage.Shared.React.Common.Image)
+local Label = require(ReplicatedStorage.Shared.React.Common.Label)
+local PromiseMotor = require(ReplicatedStorage.Shared.Util.PromiseMotor)
+local React = require(ReplicatedStorage.Packages.React)
+local Sift = require(ReplicatedStorage.Packages.Sift)
+local UseMotor = require(ReplicatedStorage.Shared.React.Hooks.UseMotor)
+
+local StartSize = UDim2.fromScale(0, 0)
+local EndSize = UDim2.fromScale(0.1, 0.05)
+
+return function(props: {
+	ImageProps: string,
+	TextProps: any,
+	StartPosition: UDim2,
+	EndPosition: UDim2,
+	OnFinished: () -> (),
+})
+	local size, sizeMotor = UseMotor(0)
+	local position, positionMotor = UseMotor(0)
+
+	React.useEffect(function()
+		local promise = PromiseMotor(sizeMotor, Flipper.Spring.new(1), function(value)
+				return value > 0.95
+			end)
+			:andThenCall(PromiseMotor, positionMotor, Flipper.Spring.new(1), function(value)
+				return value > 0.975
+			end)
+			:andThenCall(PromiseMotor, sizeMotor, Flipper.Spring.new(0), function(value)
+				return value < 0.05
+			end)
+			:andThenCall(props.OnFinished)
+
+		return function()
+			promise:cancel()
+		end
+	end, { props.StartPosition, props.EndPosition })
+
+	return React.createElement(Container, {
+		AnchorPoint = Vector2.new(0.5, 0.5),
+		Position = position:map(function(value)
+			return props.StartPosition:Lerp(props.EndPosition, value)
+		end),
+		Size = size:map(function(value)
+			return StartSize:Lerp(EndSize, value)
+		end),
+		SizeConstraint = Enum.SizeConstraint.RelativeXX,
+	}, {
+		Text = React.createElement(Label, props.TextProps),
+
+		Image = React.createElement(
+			Image,
+			Sift.Dictionary.merge({
+				AnchorPoint = Vector2.new(1, 0),
+				SizeConstraint = Enum.SizeConstraint.RelativeYY,
+			}, props.ImageProps)
+		),
+	})
+end
---
+++ src/shared/React/NumberPopups/NumberPopup.story.lua
@@ -0,0 +1,67 @@
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+
+local Container = require(ReplicatedStorage.Shared.React.Common.Container)
+local CurrencyDefs = require(ReplicatedStorage.Shared.Defs.CurrencyDefs)
+local Guid = require(ReplicatedStorage.Shared.Util.Guid)
+local NumberPopup = require(ReplicatedStorage.Shared.React.NumberPopups.NumberPopup)
+local React = require(ReplicatedStorage.Packages.React)
+local ReactRoblox = require(ReplicatedStorage.Packages.ReactRoblox)
+local Sift = require(ReplicatedStorage.Packages.Sift)
+local TextStroke = require(ReplicatedStorage.Shared.React.Util.TextStroke)
+
+local function randomPoint()
+	return Vector2.new(math.random(0, 500), math.random(0, 500))
+end
+
+local function element(props)
+	local popups, setPopups = React.useState({})
+
+	React.useEffect(function()
+		local active = true
+
+		task.spawn(function()
+			while active do
+				setPopups(function(oldPopups)
+					return Sift.Dictionary.set(oldPopups, Guid(), {
+						Number = math.random(1, 100),
+						StartPosition = UDim2.fromScale(math.random(), math.random()),
+						EndPosition = UDim2.fromScale(0.5, 0.1),
+					})
+				end)
+
+				task.wait(0.5)
+			end
+		end)
+
+		return function()
+			active = false
+		end
+	end, {})
+
+	return React.createElement(
+		Container,
+		nil,
+		Sift.Dictionary.map(popups, function(data, guid)
+			return React.createElement(NumberPopup, {
+				TextProps = { Text = TextStroke(`+{data.Number}`) },
+				ImageProps = { Image = CurrencyDefs.Primary.Image },
+				StartPosition = data.StartPosition,
+				EndPosition = data.EndPosition,
+				OnFinished = function()
+					setPopups(function(oldPopups)
+						return Sift.Dictionary.removeKey(oldPopups, guid)
+					end)
+				end,
+			})
+		end)
+	)
+end
+
+return function(target)
+	local root = ReactRoblox.createRoot(target)
+	root:render(React.createElement(element, {}))
+
+	return function()
+		root:unmount()
+	end
+end
